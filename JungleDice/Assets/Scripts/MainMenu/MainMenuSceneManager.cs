472318f01212bd097820f836a669134bc90ea9ff
woong <dndxo92@gmail.com>
2026-08-27 00:46:08 +0900

    [#44] JD [자동] 메인메뉴 모험탭 덱 미리보기 > feat(mainmenu): 모험탭 덱 미리보기 카드 3개 연동
    친구탭 UserDataChanged 발행 시 모험탭 덱 카드도 함께 갱신되도록 구독 추가

--- JungleDice/Assets/Scripts/MainMenu/MainMenuSceneManager.cs
+++ JungleDice/Assets/Scripts/MainMenu/MainMenuSceneManager.cs
@@ -2,6 +2,8 @@ using JungleDice.Core;
 using JungleDice.Core.Event;
 using JungleDice.Core.Settings;
 using JungleDice.Core.UI;
+using JungleDice.Core.User;
+using JungleDice.InGame;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,6 +15,7 @@ namespace JungleDice.MainMenu
         [SerializeField] private Button _battleButton;
         [SerializeField] private Button _optionButton;
         [SerializeField] private Transform _canvasTransform;
+        [SerializeField] private Friend[] _deckCards; // 모험탭 덱 미리보기 3개, UserData.Friends 인덱스와 1:1
 
         private OptionPanel _optionPanel;
         private readonly CompositeDisposable _subs = new();
@@ -25,6 +28,16 @@ namespace JungleDice.MainMenu
             _soloButton.onClick.AddListener(() => OnPlayButtonClicked(GameType.Solo));
             _battleButton.onClick.AddListener(() => OnPlayButtonClicked(GameType.Battle));
             _optionButton.onClick.AddListener(_optionPanel.Show);
+
+            RefreshDeckCards();
+            _subs.Add(EventBus.Subscribe<UserDataChanged>(_ => RefreshDeckCards()));
+        }
+
+        private void RefreshDeckCards()
+        {
+            var friends = UserManager.Current.Friends; // 항상 3개(UserData 기본값이자 SetFriends 호출부의 불변 조건)
+            for (int i = 0; i < _deckCards.Length; i++)
+                _deckCards[i].SetKey(friends[i]);
         }
 
         private void OnPlayButtonClicked(GameType type)
